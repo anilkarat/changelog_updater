97c334f20405d976978b51eee09fafc3cd58bead
Anil karat <anilkarat@voiro.in>
2016-05-13 11:51:12 +0530

    updating changelog

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='changelog-updater',
     packages=['changelog_updater'],
     scripts=['changelog_updater/changelog_script.py'],
-    version='0.1.26',
+    version='0.1.27',
     install_requires=[],
     author='Ankit Richariya',
     author_email='ankit.richariya@voiro.com',
